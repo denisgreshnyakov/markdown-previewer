4253f2dace8505c1f0abbe374e0b9a7dd24b0a17
Denis <denisgreshnaykov@gmail.com>
2023-02-20 00:12:00 +0500

    add blocks editor and preview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { marked } from "marked";
+import React, { useState, useEffect } from "react";
+
+const parse = (text) => {
+  document.getElementById("preview").innerHTML = "";
+  let str = document.getElementById("editor").value.split("\n");
+  const arr = [];
+  str.forEach((value) => {
+    if (value !== "") {
+      arr.push(marked.parse(value));
+    }
+  });
+  arr.forEach((value) => {
+    document.getElementById("preview").innerHTML += value;
+  });
+};
 
 function App() {
+  const [text] = useState(`# Welcome to my React Markdown Previewer!`);
+
+  useEffect(() => {
+    parse(text);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="block-editor">
+        <div className="top-panel">
+          Editor
+          <button>развернуть</button>
+        </div>
+        <textarea onChange={parse} id="editor">
+          {text}
+        </textarea>
+      </div>
+
+      <div className="block-preview">
+        <div className="top-panel">
+          Preview
+          <button>развернуть</button>
+        </div>
+        <div id="preview"></div>
+      </div>
     </div>
   );
 }
