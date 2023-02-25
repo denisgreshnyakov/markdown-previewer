5221c0e21054cdc1c19c98a7337d6de6d598dec9
Denis <denisgreshnaykov@gmail.com>
2023-02-26 03:19:30 +0500

    use ReactMarkdown SyntaxHighlighter FontAwesomeIcon, fix windows and other

--- src/App.js
+++ src/App.js
@@ -1,46 +1,102 @@
 import "./App.css";
-import { marked } from "marked";
+
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPen,
+  faFileLines,
+  faMaximize,
+  faDownLeftAndUpRightToCenter,
+} from "@fortawesome/free-solid-svg-icons";
+
 import React, { useState, useEffect } from "react";
 
-const parse = (text) => {
-  document.getElementById("preview").innerHTML = "";
-  let str = document.getElementById("editor").value.split("\n");
-  const arr = [];
-  str.forEach((value) => {
-    if (value !== "") {
-      arr.push(marked.parse(value));
-    }
-  });
-  arr.forEach((value) => {
-    document.getElementById("preview").innerHTML += value;
-  });
-};
+function getStorageValue(key) {
+  // getting stored value
+  const saved = localStorage.getItem(key);
+  const initial = JSON.parse(saved);
+  return initial || null;
+}
 
 function App() {
-  const [text] = useState(`# Welcome to my React Markdown Previewer!`);
+  const [text, setText] = useState(getStorageValue("text"));
+
+  const [showEditor, setShowEditor] = useState(false);
+  const [showPreviewer, setShowPreviewer] = useState(false);
 
   useEffect(() => {
-    parse(text);
-  });
+    localStorage.setItem("text", JSON.stringify(text));
+  }, [text]);
 
   return (
     <div className="App">
-      <div className="block-editor">
+      <div
+        className="block-editor"
+        style={{ display: showPreviewer ? "none" : "block" }}
+      >
         <div className="top-panel">
-          Editor
-          <button>развернуть</button>
+          <div>
+            <FontAwesomeIcon className="icon" icon={faPen} />
+            Editor
+          </div>
+          <FontAwesomeIcon
+            className="deploy"
+            onClick={() => setShowEditor(!showEditor)}
+            icon={showEditor ? faDownLeftAndUpRightToCenter : faMaximize}
+          />
         </div>
-        <textarea onChange={parse} id="editor">
-          {text}
-        </textarea>
+        <textarea
+          style={{
+            minHeight: showEditor ? "80vh" : "20vh",
+            resize: showEditor ? "none" : "vertical",
+          }}
+          defaultValue={text}
+          onChange={(e) => setText(e.target.value)}
+          id="editor"
+        />
       </div>
 
-      <div className="block-preview">
+      <div
+        className="block-preview"
+        style={{ display: showEditor ? "none" : "block" }}
+      >
         <div className="top-panel">
-          Preview
-          <button>развернуть</button>
+          <div>
+            <FontAwesomeIcon className="icon" icon={faFileLines} />
+            Previewer
+          </div>
+          <FontAwesomeIcon
+            className="deploy"
+            onClick={() => setShowPreviewer(!showPreviewer)}
+            icon={faMaximize}
+          />
         </div>
-        <div id="preview"></div>
+        <div id="preview">
+          {
+            <ReactMarkdown
+              children={text}
+              remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  return (
+                    <SyntaxHighlighter
+                      children={String(children).replace(/\n$/, "")}
+                      style={dark}
+                      language="javascript"
+                      PreTag={inline ? "code" : "div"}
+                      {...props}
+                    />
+                  );
+                },
+              }}
+            ></ReactMarkdown>
+          }
+        </div>
       </div>
     </div>
   );
